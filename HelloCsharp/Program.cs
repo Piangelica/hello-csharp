f1bc9a714d79d851d0803f17a2cbba7140af3976
PC <PC@DESKTOP-LUEVN40>
2022-03-31 15:51:00 +0200

    Aggiungere i file di progetto.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Benvenuto!");
+System.Console.WriteLine("Gennaio");
+System.Console.WriteLine("Febbraio");
+System.Console.WriteLine("Marzo");
+System.Console.WriteLine("Aprile");
+System.Console.WriteLine("Maggio");
+System.Console.WriteLine("Giugno");
+System.Console.WriteLine("Luglio");
+System.Console.WriteLine("Agosto");
+System.Console.WriteLine("Settembre");
+System.Console.WriteLine("Ottobre");
+System.Console.WriteLine("Novembre");
+System.Console.WriteLine("Dicembre");
+System.Console.WriteLine("Inserisci il tuo mese preferito:");
+var testo = Console.ReadLine();
+
+System.Console.WriteLine("Grazie, arrivederci!");
